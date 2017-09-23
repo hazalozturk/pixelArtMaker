0ba98ebf6e850f346876928b0775ef504d0506ce
Hazal Ozturk <hazalozturk@Doruks-MacBook-Pro.local>
2017-09-23 13:25:49 -0400

    Fire an event upon clicking on the table cell

--- functions.js
+++ functions.js
@@ -5,14 +5,20 @@ $( ".submit" ).click(function(e) {
   makeGrid(row, column);
 });
 
+$(document).on("click", "#canvas", function(e) {
+  e.preventDefault();
+  let color = $(".colorPicker").val();
+  $(e.target).css('background-color', color);
+});
+
 function makeGrid(rows, cols){
   $("#box").empty();
 	mytable = $('<table></table>').attr({ id: "canvas" });
 	let tr = [];
 	for (var i = 0; i < rows; i++) {
-		let row = $('<tr></tr>').attr({ class: ["cell"] }).appendTo(mytable);
+		let row = $('<tr></tr>').attr({ class: "cell" }).appendTo(mytable);
 		for (let j = 0; j < cols; j++) {
-			$('<td></td>').attr({ class: ["cell"] }).appendTo(row);
+			$('<td></td>').attr({ class: "cell" }).appendTo(row);
 		}
 	}
 	mytable.appendTo("#box");
